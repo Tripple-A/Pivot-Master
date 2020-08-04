2a30b997f358e33358920758daf5c9cc3cf6a27b
Tripple-A <biodun9@gmail.com>
2020-08-04 22:53:45 +0100

    added one liner for attr readers

--- lib/pivot/person.rb
+++ lib/pivot/person.rb
@@ -1,20 +1,13 @@
 require 'logger'
 module Pivot
   class Person
+    attr_reader :email, :first_name, :last_name, :items
     def initialize(details)
       @email = details[:email]
       @first_name = details[:first_name]
       @last_name = details[:last_name]
       @items = []
-    end
-
-    attr_reader :email
-
-    attr_reader :first_name
-
-    attr_reader :last_name
-
-    attr_reader :items
+    end 
 
     def add_item(item)
       @items << item
